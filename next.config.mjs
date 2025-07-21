97ddc80cebee68e8a99a8c8b943f860f3dc94116
cbrown <colyn.brown@gmail.com>
2025-07-20 21:36:58 -0400

    feat(config): add webpack configuration to ignore specific plugins

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,14 @@ import { withPayload } from "@payloadcms/next/withPayload"
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
+  webpack: (config, { webpack }) => {
+    config.plugins.push(
+      new webpack.IgnorePlugin({
+        resourceRegExp: /^pg-native$|^cloudflare:sockets$/,
+      }),
+    )
+    return config
+  },
   images: {
     remotePatterns: [
       {
