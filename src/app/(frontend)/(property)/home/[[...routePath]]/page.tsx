093edbfdfbdae5dfac5cd91c510a440d28d670b4
Salek Uddin Sheikh <devsalek@gmail.com>
2025-07-28 20:08:26 +0600

    init

--- src/app/(frontend)/(property)/home/[[...routePath]]/page.tsx
+++ src/app/(frontend)/(property)/home/[[...routePath]]/page.tsx
@@ -21,7 +21,7 @@ export async function generateMetadata({ params }: { params: Promise<{ routePath
   }
 
   return {
-    metadataBase: new URL(SERVER_URL || "http://localhost:8181"),
+    metadataBase: new URL(SERVER_URL || "http://localhost:3000"),
     alternates: {
       canonical: property.url,
     },
